edc1196036a1cd2d2e5d32123274c1f44cef9c58
Martien Dermawan Tanama <martiendt@gmail.com>
2024-11-19 13:27:40 +0700

    x

--- src/views/layouts/AppFooter.vue
+++ src/views/layouts/AppFooter.vue
@@ -4,11 +4,21 @@
   >
     <div class="content py-20 font-size-xs clearfix">
       <div class="float-right">
-        Crafted with <i class="fa fa-heart text-pulse" /> by <a
-          class="font-w600"
-          href="javascript:void(0)"
-          target="_blank"
-        >RED POINT</a>
+        <div>
+          <div>
+            <a href="button" class="btn btn-sm">
+              <img src="/whatsapp.png" alt="">
+              Contact Us
+            </a>
+          </div>
+          <div>
+            Crafted with <i class="fa fa-heart text-pulse" /> by <a
+              class="font-w600"
+              href="javascript:void(0)"
+              target="_blank"
+            >RED POINT</a>
+          </div>
+        </div>
       </div>
       <div class="float-left">
         VERSION {{ version }} | &copy; 2020
