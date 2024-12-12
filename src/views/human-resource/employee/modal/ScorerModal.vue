58e6452e06c2a02294efe84aee2fccef699b324a
Martien Dermawan Tanama <martiendt@gmail.com>
2024-12-12 13:41:00 +0700

    fix scorer

--- src/views/human-resource/employee/modal/ScorerModal.vue
+++ src/views/human-resource/employee/modal/ScorerModal.vue
@@ -7,10 +7,23 @@
         overlay-theme="dark"
         @close="onClose()"
       >
-        <div class="list-group mb-20">
-          <template v-for="(user, index) in users">
+        <input
+          ref="searchText"
+          v-model="searchText"
+          type="text"
+          class="form-control"
+          placeholder="Search..."
+          @keydown.enter.prevent=""
+        >
+        <hr>
+        <div v-if="isLoading">
+          <h3 class="text-center">
+            Loading ...
+          </h3>
+        </div>
+        <div v-else class="list-group mb-20">
+          <template v-for="(user, index) in options" :key="index">
             <a
-              :key="index"
               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
               href="javascript:void(0)"
               @click="onSubmitScorer(user, index)"
@@ -18,6 +31,12 @@
               <span><i class="fa fa-fw fa-hand-o-right mr-5" /> {{ user.first_name + ' ' + user.last_name }}</span>
             </a>
           </template>
+        </div>
+        <div
+          v-if="searchText && options.length == 0 && !isLoading"
+          class="alert alert-info text-center"
+        >
+          {{ $t('searching not found', [searchText]) | capitalize }} <br>
         </div>
       </sweet-modal>
     </form>
@@ -40,7 +59,10 @@ export default {
   },
   data () {
     return {
-      scorer: null
+      scorer: null,
+      searchText: '',
+      options: [],
+      isLoading: false
     }
   },
   computed: {
@@ -49,13 +71,44 @@ export default {
   watch: {
     'scorer' () {
       this.$emit('value', this.scorer)
-    }
+    },
+    searchText: debounce(function () {
+      this.search()
+    }, 300),
   },
   created () {
-    this.get()
+    this.get({ params: { limit: 99999 }})
   },
   methods: {
     ...mapActions('masterUser', ['get']),
+    search () {
+      this.isLoading = true
+      this.get({
+        params: {
+          sort_by: 'name',
+          limit: 50,
+          filter_like: {
+            first_name: this.searchText,
+            last_name: this.searchText
+          }
+        }
+      }).then(response => {
+        this.options = []
+        response.data.map((key, value) => {
+          const obj = {
+            id: key.id,
+            first_name: key.first_name,
+            last_name: key.last_name
+          }
+          this.options.push({
+            ...obj
+          })
+        })
+        this.isLoading = false
+      }).catch(error => {
+        this.isLoading = false
+      })
+    },
     open () {
       this.$refs.modal.open()
     },
